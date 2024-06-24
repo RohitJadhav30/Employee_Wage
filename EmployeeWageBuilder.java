8bef3e723eee946ecff651ab05f42941d4932221
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-06-24 17:06:01 +0530

    Checking employee's status

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,15 @@
 public class EmployeeWageBuilder{
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Wage Builder Program");
+        int IS_FULL_TIME = 1;
+
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        System.out.println(empCheck);
+
+        if(empCheck == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
     }
 }
